41ada072d1bcadb770a84c4a059c2f0fb347197d
mehmetpekcan <pekcanmehmett@gmail.com>
2023-01-13 18:23:28 +0300

    Update head file

--- app/head.jsx
+++ app/head.jsx
@@ -3,6 +3,10 @@ export default function Head() {
     <>
       <title>Imaginer</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta
+        name="description"
+        content="Imaginer is a simple image generator that use your prompt to generate AI based images."
+      />
     </>
   );
 }
