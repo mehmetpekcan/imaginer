8f60d6ffc083f738f8aaa543e06da8cfb3231050
mehmetpekcan <pekcanmehmett@gmail.com>
2023-01-13 18:21:36 +0300

    Add head file

--- app/head.jsx
+++ app/head.jsx
@@ -1,7 +1,7 @@
 export default function Head() {
   return (
     <>
-      <title>Create Next App</title>
+      <title>Imaginer</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
     </>
   );
