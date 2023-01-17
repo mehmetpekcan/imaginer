724dd3a0447cbe8e77b5a884f120e652890ead89
mehmetpekcan <pekcanmehmett@gmail.com>
2023-01-17 23:42:38 +0300

    Add forgotten import

--- middleware.js
+++ middleware.js
@@ -1,11 +1,13 @@
 import { NextResponse } from "next/server";
+import { applyRateLimit } from "./utils/rate-limiter";
 
 export async function middleware(request) {
   const response = NextResponse.next();
 
   try {
     await applyRateLimit(request, response);
-  } catch {
+  } catch (error) {
+    console.log(error);
     return new NextResponse("Too Many Request", { status: 429 });
   }
 
